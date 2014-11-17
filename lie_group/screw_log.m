4c4c8c872f93a9f009efbcddbf62a152d2acb884
Ming Ruan <mhr@cmu.edu>
2014-11-17 18:36:22 -0500

    Critical: fixed a big in screw_log to deal with theta=pi

--- lie_group/screw_log.m
+++ lie_group/screw_log.m
@@ -11,10 +11,20 @@ end
 theta = acos(val);
 
 if 0==theta
-    w_hat = zeros(3,3);
+    w = zeros(3,1);
+elseif pi==theta
+    K2 = 0.5*(R+eye(3));
+    w = sqrt(diag(K2));
+    if     K2(1,1) ~= 0 % if w1 ~=0
+        w = theta*w.*[      1      ; sign(K2(1,2)); sign(K2(1,3))];
+    elseif K2(2,2) ~= 0 % if w2 ~=0
+        w = theta*w.*[sign(K2(2,1));      1       ; sign(K2(2,3))];
+    else                % if w3 ~=0
+        w = theta*w.*[sign(K2(3,1)); sign(K2(3,2));      1       ];
+    end
 else
     w_hat = (R-R')/(2*sin(theta))*theta;
+    w = so3_gla(w_hat);
 end
 
-w = so3_gla(w_hat);
 end
