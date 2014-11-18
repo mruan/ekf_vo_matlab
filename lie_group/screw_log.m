760951eeda534a2fd1f883489ef088009718de57
Ming Ruan <mhr@cmu.edu>
2014-11-18 12:13:38 -0500

    Critical: Rewrote screw_log to be more robust

--- lie_group/screw_log.m
+++ lie_group/screw_log.m
@@ -1,30 +1,55 @@
-function [w] = screw_log(R)
+function [ w ] = screw_log( R )
+% Work based on Edward Rosten's C++ implemention of SO3 class
+% http://www.edwardrosten.com/cvd/toon/html-user/so3_8h_source.html
 
-val = 0.5*(trace(R)-1);
+cos_t = 0.5*(trace(R)-1);
 
-if val > 1.0
-    val = 1.0;
-elseif val < -1.0
-    val =-1.0;
+if cos_t > 1.0
+    cos_t = 1.0;
+elseif cos_t < -1.0
+    cos_t =-1.0;
 end
 
-theta = acos(val);
+w = 0.5*[R(3,2) - R(2,3); R(1,3) - R(3,1); R(2,1) - R(1,2)];
 
-if 0==theta
-    w = zeros(3,1);
-elseif pi==theta
-    K2 = 0.5*(R+eye(3));
-    w = sqrt(diag(K2));
-    if     K2(1,1) ~= 0 % if w1 ~=0
-        w = theta*w.*[      1      ; sign(K2(1,2)); sign(K2(1,3))];
-    elseif K2(2,2) ~= 0 % if w2 ~=0
-        w = theta*w.*[sign(K2(2,1));      1       ; sign(K2(2,3))];
-    else                % if w3 ~=0
-        w = theta*w.*[sign(K2(3,1)); sign(K2(3,2));      1       ];
+sin_t_abs = norm(w);
+
+if cos_t > 1/sqrt(2) 
+    % a ~ [0 ~ Pi/4] => use asin
+    if sin_t_abs >0
+        w = w * asin(sin_t_abs)/sin_t_abs;
+        % else w is returned directly -> e.g. w = [0 0 0] for R==I
     end
-else
-    w_hat = (R-R')/(2*sin(theta))*theta;
-    w = so3_gla(w_hat);
+elseif cos_t > -1/sqrt(2) 
+    % a ~ [Pi/4 ~ 3Pi/4] => use acos, w/o antisymmetric part
+    t = acos(cos_t);
+    w = w * t / sin_t_abs;
+else % use symmetric part
+   % antisymmetric part vanishes, but still large rotation, need
+   % information from symmetric part
+    t = pi - asin(sin_t_abs);
+    d = [R(1,1) - cos_t; R(2,2) - cos_t; R(3,3)-cos_t]; % diag(R)-cos_t
+    D = d.^2;
+    if D(1) > D(2) && D(1) > D(3) % 1st is largest, fill 1st column
+        r2 = [d(1); ...
+              0.5*(R(1,2)+R(2,1));...
+              0.5*(R(1,3)+R(3,1))];
+    elseif D(1) > D(2)            % 2nd is largest, fill 2nd column
+        r2 = [0.5*(R(2,1)+R(1,2));...
+              d(2);...
+              0.5*(R(2,3)+R(3,2))];
+    else
+        r2 = [0.5*(R(3,1)+R(1,3));...
+              0.5*(R(3,2)+R(2,3));...
+              d(3)];
+    end
+    
+    if dot(r2, w) < 0 % Flip if point in the wrong direction
+       r2 = -r2; 
+    end
+    r2 = r2/norm(r2);
+    w = t*r2;
 end
 
 end
+
--- lie_group/so3_alg.m
+++ lie_group/so3_alg.m
@@ -2,5 +2,7 @@ function [Mx] = so3_alg(v)
 
 assert(numel(v) == 3, 'Input vector must contain exactly 3 elements');
 
-Mx = [0 -v(3) v(2); v(3) 0 -v(1); -v(2) v(1) 0];
+Mx = [   0 -v(3)  v(2);...
+      v(3)     0 -v(1);...
+     -v(2)  v(1)    0];
 end
