ea7e01d05fb4bc2bb660213e909a9ebe525b12ee
Ming Ruan <mhr@cmu.edu>
2014-11-23 16:39:49 -0500

    so3_gla supports slightly off skew matrix

--- lie_group/so3_gla.m
+++ lie_group/so3_gla.m
@@ -1,7 +1,7 @@
 function [vec] = so3_gla(m)
 % inverse of alg operator for so3 (alg -> gla)
 
-assert(norm(m+transpose(m)) == 0, 'm is not a symmetric-skew matrix');
+assert(norm(m+transpose(m)) < 1e-10, 'm is not a symmetric-skew matrix');
 
-vec = [m(3,2); m(1,3); m(2,1)];
+vec = 0.5*[m(3,2)-m(2,3); m(1,3)-m(3,1); m(2,1)-m(1,2)];
 end
